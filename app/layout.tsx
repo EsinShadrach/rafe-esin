b538a868b40f75f5b1ed8f83b1d3fee4a607b336
EsinShadrach <esinshadrachrafe@gmail.com>
2023-12-03 22:15:59 +0100

    Added Meta Data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,60 @@ import { cn } from "~/utils/class-name";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Rafe - Esin",
-  description: "Rafe Esin's Portfolio Shadrach's Esin Portfolio",
-  authors: [{ name: "Rafe Esin" }, { name: "Shadrach Esin" }],
-  creator: "Rafe Esin",
+  metadataBase: new URL("https://rafe-esin.vercel.app"),
+  title: "Shadrach Esin - Portfolio",
+  description:
+    "Welcome to the portfolio of Shadrach Esin, showcasing creative works and projects. Explore my skills, experience, and accomplishments.",
+  applicationName: "Shadrach Esin Portfolio",
+  authors: [{ name: "Shadrach Esin", url: "https://rafe-esin.vercel.app" }],
+  keywords: [
+    "Shadrach Esin",
+    "Portfolio",
+    "Projects",
+    "Design",
+    "Development",
+    "Creative Works",
+  ],
+  referrer: "origin",
+  themeColor: "#3498db",
+  openGraph: {
+    type: "website",
+    url: "https://rafe-esin.vercel.app",
+    title: "Shadrach Esin - Portfolio",
+    description:
+      "Welcome to the portfolio of Shadrach Esin, showcasing creative works and projects.",
+    siteName: "Shadrach Esin Portfolio",
+    images: [
+      {
+        url: "https://rafe-esin.vercel.app/og-image.png",
+        alt: "Shadrach Esin Portfolio",
+      },
+    ],
+    locale: "en_US",
+  },
+  twitter: {
+    site: "@rafeesin",
+    creator: "@rafeesin",
+    title: "Shadrach Esin - Portfolio",
+    description:
+      "Welcome to the portfolio of Shadrach Esin, showcasing creative works and projects.",
+  },
+  appleWebApp: {
+    capable: true,
+    title: "Shadrach Esin Portfolio",
+    statusBarStyle: "black-translucent",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  abstract:
+    "Explore the creative journey and professional accomplishments of Shadrach Esin through an impressive portfolio of design, development, and other creative projects.",
+  category: "Portfolio",
+  classification: "Personal Portfolio",
+  other: {
+    industry: ["Web Development", "App Development"],
+    audience: "Clients, Employers, Collaborators",
+  },
 };
 
 export default function RootLayout({
